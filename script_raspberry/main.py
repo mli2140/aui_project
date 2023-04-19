30009370fc9a87b25f5762cdb823bb30f477e110
mli2140 <100526761+mli2140@users.noreply.github.com>
2023-04-19 19:16:43 +0200

    Update main.py

--- script_raspberry/main.py
+++ script_raspberry/main.py
@@ -1,69 +1,88 @@
 #Libraries
 import RPi.GPIO as GPIO
+import threading
 import time
  
 #GPIO Mode (BOARD / BCM)
 GPIO.setmode(GPIO.BCM)
  
-#set GPIO Pins
+#set GPIO Pins Ultrasonic 1
 GPIO_TRIGGER = 11
 GPIO_ECHO = 12
 
-GPIO_TRIGGER_2 = 24
-GPIO_ECHO_2 = 18
+#set GPIO Pins Ultrasonic 2
+GPIO_TRIGGER2 = 18
+GPIO_ECHO2 = 13
 
-GPIO.setwarnings(False)
-
-GPIO.setup(GPIO_TRIGGER_2, GPIO.OUT)
-GPIO.setup(GPIO_ECHO_2, GPIO.IN)
- 
- 
-#set GPIO direction (IN / OUT)
+#set GPIO direction (IN / OUT) Ultrasonic 1
 GPIO.setup(GPIO_TRIGGER, GPIO.OUT)
 GPIO.setup(GPIO_ECHO, GPIO.IN)
+#set GPIO direction (IN / OUT) Ultrasonic 2
+GPIO.setup(GPIO_TRIGGER2, GPIO.OUT)
+GPIO.setup(GPIO_ECHO2, GPIO.IN)
+
+elevator_dist = 10
+stairs_dist = 10
+
+elevator_count = 0
+stairs_count = 0
+
  
-def distance():
+def distance(trigger, echo):
     # set Trigger to HIGH
-    GPIO.output(GPIO_TRIGGER, True)
+    GPIO.output(trigger, True)
  
     # set Trigger after 0.01ms to LOW
     time.sleep(0.00001)
-    GPIO.output(GPIO_TRIGGER, False)
+    GPIO.output(trigger, False)
  
     StartTime = time.time()
     StopTime = time.time()
  
     # save StartTime
-    while GPIO.input(GPIO_ECHO) == 0:
+    while GPIO.input(echo) == 0:
         StartTime = time.time()
  
     # save time of arrival
-    while GPIO.input(GPIO_ECHO) == 1:
+    while GPIO.input(echo) == 1:
         StopTime = time.time()
  
     # time difference between start and arrival
     TimeElapsed = StopTime - StartTime
-    # multiply with the sonic speed (34300 cm/s)
-    # and divide by 2, because there and back
+
     distance = (TimeElapsed * 34300) / 2
- 
+
     return distance
 
- 
-#   
+def async countPerson(actual, dist, index):
+    if(actual < dist):
+        if(index == 1):
+            global stairs_count
+            stairs_count += 1
+        else:
+            global elevator_count
+            elevator_count += 1
+        time.sleep(2)
  
 if __name__ == '__main__':
     try:
         while True:
-            dist = distance()
-            #dist2 = distance2()
-            # if (dist < 15):
-            print ("Measured Distance = %.1f cm" % dist)
-            time.sleep(0.1)
- 
-        # Reset by pressing CTRL + C
+            actual_elevator = distance(GPIO_TRIGGER, GPIO_ECHO)
+            actual_stairs = distance(GPIO_TRIGGER2, GPIO_ECHO2)
+            
+            await countPerson(actual_stairs, stairs_dist, 1)
+            #s.start()
+            
+            await countPerson(actual_elevator, elevator_dist, 0)
+            #e.start()
+            
+            print(actual_elevator)
+            print(actual_stairs)
+            
+            print(elevator_count)
+            print(stairs_count)
+                
     except KeyboardInterrupt:
         print("Measurement stopped by User")
         GPIO.cleanup()
-
 
